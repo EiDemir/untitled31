1e08be148dc54fec8a12015d2c693e31bff9d0ee
EiDemir <ahizadiii@gmail.com>
2023-07-03 23:48:40 +0300

    update

--- src/app/(shop)/products/[category]/layout.tsx
+++ src/app/(shop)/products/[category]/layout.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import patternImage from "../../../../../public/pattern.svg";
 import {prisma} from "@/libs/prisma";
 
-export const dynamicParams = false;
+const dynamicParams = false;
+export {dynamicParams};
 
 export async function generateStaticParams() {
     const categories = await prisma.category.findMany({
