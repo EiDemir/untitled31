a341ffcfefca7044faf378181fa0f1e30727c24f
EiDemir <ahizadiii@gmail.com>
2023-07-03 15:28:04 +0300

    update

--- src/app/(home)/@bestSelling/loading.tsx
+++ src/app/(home)/@bestSelling/loading.tsx
@@ -17,13 +17,13 @@ export default function BestSellingLoading() {
                     <div className='rounded-full bg-slate-300 w-36 h-3'/>
                     <div className='rounded-full bg-slate-300 w-10 h-3'/>
                 </div>
-                <div className='gap-y-3 flex flex-col w-full'>
+                <div className='gap-y-3 hidden flex-col w-full sm:flex'>
                     <div className='rounded-xl w-full aspect-[1333/2000] bg-slate-300'/>
                     <div className='rounded-full bg-slate-300 w-10 h-2'/>
                     <div className='rounded-full bg-slate-300 w-36 h-3'/>
                     <div className='rounded-full bg-slate-300 w-10 h-3'/>
                 </div>
-                <div className='gap-y-3 flex flex-col w-full'>
+                <div className='gap-y-3 hidden flex-col w-full lg:flex'>
                     <div className='rounded-xl w-full aspect-[1333/2000] bg-slate-300'/>
                     <div className='rounded-full bg-slate-300 w-10 h-2'/>
                     <div className='rounded-full bg-slate-300 w-36 h-3'/>
