969defe63be2685f5f4e1756ad36cdd62dd7a07e
EiDemir <ahizadiii@gmail.com>
2023-07-08 04:29:13 +0300

    update

--- src/app/(shop)/products/[category]/loading.tsx
+++ src/app/(shop)/products/[category]/loading.tsx
@@ -5,8 +5,8 @@ import ProductItemLoading from "@/components/ui/loading/ProductItemLoading";
 
 export default function CategoryLoading() {
     return (
-        <div className='w-3/4 grid grid-cols-3 gap-7'>
-            {_.range(15).map(num => <ProductItemLoading key={num}/>)}
+        <div className='w-4/5 grid grid-cols-4 gap-5'>
+            {_.range(24).map(num => <ProductItemLoading key={num}/>)}
         </div>
     );
 }
