8f0e947a1e26d09c69b03cda4a2ee9603edab49e
EiDemir <ahizadiii@gmail.com>
2023-07-12 01:22:52 +0300

    update

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -54,19 +54,19 @@ const ProductItem = forwardRef(({imageLink, category, title, price, direction, i
                  onMouseLeave={() => setIsHovered(false)}
                  className='relative overflow-hidden rounded-xl'>
                 <Link prefetch={false} href={'/product/' + id}>
-                    <Image className="rounded-xl" src={imageLink} alt="Product's picture" width={1333}
+                    <Image className="rounded-md" src={imageLink} alt="Product's picture" width={1333}
                            height={2000}/>
                 </Link>
                 <motion.div
-                    className='rounded-xl flex text-white divide-x divide-[#222222] text-xs absolute inset-x-0 bg-[#222222]/50 backdrop-blur-lg h-10 bottom-1 mx-1'
+                    className='rounded-md flex text-white divide-x divide-[#222222] text-xs absolute inset-x-0 bg-[#222222]/50 backdrop-blur-lg h-10 bottom-1 mx-1'
                     initial={false}
                     animate={{
                         opacity: isHovered ? 1 : 0,
                         y: isHovered ? 0 : 20
                     }}>
-                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-l-xl'>ADD TO CARD
+                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-l-md'>ADD TO CARD
                     </button>
-                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-r-xl'>QUICK VIEW
+                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-r-md'>QUICK VIEW
                     </button>
                 </motion.div>
             </div>
--- src/components/product/WishlistItem.tsx
+++ src/components/product/WishlistItem.tsx
@@ -33,16 +33,16 @@ export default function WishlistItem(
                     className='disabled:bg-gray-400 absolute top-2 left-2 w-10 p-1.5 bg-white rounded-xl'>
                     <XMarkIcon className='text-black '/>
                 </button>
-                <Image className="aspect-[3/4] object-cover rounded-xl" src={imageLink} alt="Product's picture" width={400}
+                <Image className="aspect-[3/4] object-cover rounded-md" src={imageLink} alt="Product's picture" width={400}
                        height={485}/>
                 <motion.div
-                    className='rounded-xl flex text-white divide-x divide-[#222222] text-xs absolute inset-x-0 bg-[#222222]/50 backdrop-blur-lg h-10 bottom-1 mx-1'
+                    className='rounded-md flex text-white divide-x divide-[#222222] text-xs absolute inset-x-0 bg-[#222222]/50 backdrop-blur-lg h-10 bottom-1 mx-1'
                     initial={false}
                     animate={{
                         opacity: isHovered ? 1 : 0,
                         y: isHovered ? 0 : 20
                     }}>
-                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-xl'>ADD TO CARD</button>
+                    <button className='transition ease-out duration-500 w-full hover:bg-black rounded-md'>ADD TO CARD</button>
                 </motion.div>
             </div>
             <div className='flex flex-col font-normal'>
