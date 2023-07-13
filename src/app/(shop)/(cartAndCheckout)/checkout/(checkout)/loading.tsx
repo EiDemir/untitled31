a89639b01bf29df5e573aedae13207d969ef9d87
EiDemir <ahizadiii@gmail.com>
2023-07-13 22:19:59 +0300

    update

---
+++ src/app/(shop)/(cartAndCheckout)/checkout/(checkout)/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+    return (
+        <div className='flex flex-col gap-y-7 w-2/3'>
+            <div className='rounded-3xl w-full ring-1 ring-[#E4E4E4] ring-inset h-44'>
+
+            </div>
+            <div className='rounded-3xl w-full ring-1 ring-[#E4E4E4] ring-inset h-44'>
+            </div>
+        </div>
+    );
+}
