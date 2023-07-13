a4ab167f42bd267091682219945543c98d67acd2
EiDemir <ahizadiii@gmail.com>
2023-07-14 00:31:45 +0300

    update

--- src/app/(shop)/products/[category]/page.tsx
+++ src/app/(shop)/products/[category]/page.tsx
@@ -1,94 +1,19 @@
-import CategoryScroll from "./CategoryScroll";
-import {prisma} from "@/libs/prisma";
-import Category from "./Category";
+import ProductsSection from "./ProductsSection";
+import {Suspense} from "react";
+import ProductItemLoading from "@/components/ui/loading/ProductItemLoading";
 import _ from 'lodash';
 
-async function getCategoryProducts(categoryName: string, page: number, colors: string[], sizes: string[], minMaxPrice: number[], sort?: string) {
-    return prisma.category.findUnique({
-        where: {
-            name: categoryName
-        }, select: {
-            products: {
-                where: {
-                    AND: [
-                        {
-                            sizes: {
-                                hasEvery: sizes.map(item => _.toLower(item))
-                            },
-                            price: minMaxPrice.length > 0 ? {
-                                gte: minMaxPrice[0],
-                                lte: minMaxPrice[1]
-                            } : {},
-                        }, ...colors.map(color => ({
-                            colors: {
-                                some: {
-                                    name: color
-                                }
-                            }
-                        }))
-                    ]
-                },
-                orderBy: sort ? {
-                    price: sort === 'htl' ? 'desc' : 'asc'
-                } : {},
-                select: {
-                    images: true,
-                    category: true,
-                    name: true,
-                    price: true,
-                    id: true
-                }, take: 24,
-                skip: (page - 1) * 24
-            }, _count: {
-                select: {
-                    products: {
-                        where: {
-                            AND: [
-                                {
-                                    sizes: {
-                                        hasEvery: sizes.map(item => _.toLower(item))
-                                    },
-                                    price: minMaxPrice.length > 0 ? {
-                                        gte: minMaxPrice[0],
-                                        lte: minMaxPrice[1]
-                                    } : {},
-                                }, ...colors.map(color => ({
-                                    colors: {
-                                        some: {
-                                            name: color
-                                        }
-                                    }
-                                }))
-                            ]
-                        }
-                    }
-                }
-            }
-        }
-    });
-}
-
-export default async function Section({params, searchParams}: {
+export default function Page({params, searchParams}: {
     params: { category: string, page?: string },
     searchParams: { page?: string, color?: string, size?: string, minPrice?: string, maxPrice?: string, sort?: string }
 }) {
-    const products = await getCategoryProducts(params.category,
-        searchParams !== undefined && searchParams.page !== undefined ? parseInt(searchParams.page) : 1,
-        !searchParams.color ? [] : searchParams.color.split(','),
-        !searchParams.size ? [] : searchParams.size.split(','),
-        !searchParams.minPrice || !searchParams.maxPrice ? [] : [parseFloat(searchParams.minPrice), parseFloat(searchParams.maxPrice)],
-        searchParams.sort);
-
-    if (!products || !products.products.length) {
-        return <div>NO PRODUCTS WERE FOUND.</div>;
-    }
-
-    if (searchParams.page)
-        return (
-            <Category products={products}/>
-        );
-
     return (
-        <CategoryScroll initialProducts={products}/>
+        <Suspense key={JSON.stringify(searchParams)} fallback={
+            <div className='w-4/5 grid grid-cols-4 gap-5'>
+                {_.range(24).map(num => <ProductItemLoading key={num}/>)}
+            </div>
+        }>
+            <ProductsSection params={params} searchParams={searchParams}/>
+        </Suspense>
     );
 }
