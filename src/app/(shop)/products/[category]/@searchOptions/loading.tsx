225703a521548c7fb743dd3d7d186fd3a6896e93
EiDemir <ahizadiii@gmail.com>
2023-07-13 23:57:57 +0300

    update

--- src/app/(shop)/products/[category]/@searchOptions/loading.tsx
+++ src/app/(shop)/products/[category]/@searchOptions/loading.tsx
@@ -1,6 +1,6 @@
 export default function SearchOptionsLoading() {
     return (
-        <div className='flex flex-col gap-y-5'>
+        <div className='flex flex-col gap-y-5 w-1/5 sticky top-20'>
             <div className='bg-slate-400 rounded-xl w-full h-10'/>
             <div className='bg-slate-400 rounded-xl w-full h-10'/>
             <div className='bg-slate-400 rounded-xl w-full h-10'/>
