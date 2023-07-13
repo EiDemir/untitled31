498ee8c592a113db434045d985f7224fe4d5a8d5
EiDemir <ahizadiii@gmail.com>
2023-07-13 23:52:30 +0300

    update

--- src/app/(shop)/(cartAndCheckout)/checkout/(checkout)/loading.tsx
+++ src/app/(shop)/(cartAndCheckout)/checkout/(checkout)/loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
         <>
             <div className='rounded-3xl w-full ring-1 ring-[#E4E4E4] bg-black ring-inset h-44'>
             </div>
-            <div className='rounded-3xl w-full ring-1 ring-[#E4E4E4] ring-inset h-44'>
+            <div className='rounded-3xl w-full ring-1 ring-[#E4E4E4] bg-black ring-inset h-44'>
             </div>
         </>
     );
