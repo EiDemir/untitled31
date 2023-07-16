dba6fe5f00b57beea8809cb4dc07f463b7da8d0a
EiDemir <ahizadiii@gmail.com>
2023-07-16 21:12:47 +0300

    update

--- src/app/(shop)/products/[category]/@searchOptions/page.tsx
+++ src/app/(shop)/products/[category]/@searchOptions/page.tsx
@@ -1,5 +1,7 @@
 import {prisma} from "@/libs/prisma";
 import SearchOptions from "@/app/(shop)/products/[category]/@searchOptions/SearchOptions";
+import {Suspense} from "react";
+import SearchOptionsLoading from "@/app/(shop)/products/[category]/@searchOptions/SearchOptionsLoading";
 
 async function getColors(category: string) {
     return prisma.color.findMany();
@@ -32,5 +34,9 @@ export default async function SearchOptionsSection({params}: {
 
     const [colors, sizes, minMaxPrices] = await Promise.all([colorsPromise, sizesPromise, minMaxPromise]);
 
-    return <SearchOptions colors={colors} sizes={sizes} minMaxPrices={minMaxPrices}/>;
+    return (
+        <Suspense fallback={<SearchOptionsLoading/>}>
+            <SearchOptions colors={colors} sizes={sizes} minMaxPrices={minMaxPrices}/>
+        </Suspense>
+    );
 }
