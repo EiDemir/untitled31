ffe988adebd4e37effadfd5ac878d3ba7271127e
Emir <59157550+EiDemir@users.noreply.github.com>
2023-07-24 20:35:21 +0300

    Update page.tsx

--- src/app/(shop)/products/[category]/page.tsx
+++ src/app/(shop)/products/[category]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({params, searchParams}: {
         <div className='md:w-4/5'>
             <SortingOptions searchParams={searchParams}/>
             <Suspense key={JSON.stringify(searchParams)} fallback={
-                <div className='grid grid-cols-4 gap-5'>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
                     {_.range(24).map(num => <ProductItemLoading key={num}/>)}
                 </div>
             }>
