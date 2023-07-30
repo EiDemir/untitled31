c06972daeae61186341beb56f0a89f9aaedbd1b9
EiDemir <ahizadiii@gmail.com>
2023-07-30 16:41:39 +0300

    update

--- src/components/ui/loading/ProductItemLoading.tsx
+++ src/components/ui/loading/ProductItemLoading.tsx
@@ -1,10 +1,15 @@
 export default function ProductItemLoading() {
     return (
-        <div className='gap-y-3.5 hidden flex-col w-full lg:flex'>
+        <div className='gap-y-3 flex-col w-full flex'>
             <div className='rounded-xl w-full aspect-[1333/2000] bg-slate-300'/>
             <div className='rounded-full bg-slate-300 w-10 h-2'/>
             <div className='rounded-full bg-slate-300 w-36 h-3'/>
             <div className='rounded-full bg-slate-300 w-10 h-3'/>
+            <div className='flex gap-x-3 pt-px'>
+                <div className='bg-slate-300 w-5 h-5 rounded-full'/>
+                <div className='bg-slate-300 w-5 h-5 rounded-full'/>
+                <div className='bg-slate-300 w-5 h-5 rounded-full'/>
+            </div>
         </div>
     );
 }
