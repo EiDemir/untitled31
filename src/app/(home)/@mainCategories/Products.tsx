125ada3c56080db4cae2b93651c5a10e9d40a109
EiDemir <ahizadiii@gmail.com>
2023-07-30 17:25:26 +0300

    update

--- src/app/(home)/@mainCategories/Products.tsx
+++ src/app/(home)/@mainCategories/Products.tsx
@@ -1,23 +1,34 @@
-'use client';
-
 import CategoryItem from "@/components/product/CategoryItem";
+import {prisma} from "@/libs/prisma";
+import getCurrentUser from "@/actions/getCurrentUser";
 
-export default function Products({products, isAuthenticated}: {
-    isAuthenticated: boolean,
-    products: {
-        images: string[],
-        name: string,
-        price: number,
-        categories: { name: string }[],
-        id: string,
-        sizes: string[],
-        colors: { name: string, hexColorCode: string }[]
-    }[]
-}) {
+const revalidate = 3600;
+
+async function getBestSellingProducts() {
+    return prisma.product.findMany({
+        select: {
+            images: true,
+            name: true,
+            price: true,
+            categories: {
+                select: {
+                    name: true
+                }
+            }, id: true,
+            colors: true,
+            sizes: true
+        }, take: 5
+    });
+}
+
+export default async function Products() {
+    const products = await getBestSellingProducts();
+    const user = await getCurrentUser();
+
     return (
         <div className='grid grid-cols-3 md:grid-cols-5 gap-4'>
             {products!.map(product => <CategoryItem
-                isAuthenticated={isAuthenticated}
+                isAuthenticated={user !== null}
                 colors={product.colors}
                 sizes={product.sizes}
                 key={product.id}
--- src/app/(home)/@mainCategories/page.tsx
+++ src/app/(home)/@mainCategories/page.tsx
@@ -1,31 +1,10 @@
-import {prisma} from "@/libs/prisma";
 import Products from "./Products";
 import Countdown from "./Countdown";
-import getCurrentUser from "@/actions/getCurrentUser";
-
-const revalidate = 3600;
-
-async function getBestSellingProducts() {
-    return prisma.product.findMany({
-        select: {
-            images: true,
-            name: true,
-            price: true,
-            categories: {
-                select: {
-                    name: true
-                }
-            }, id: true,
-            colors: true,
-            sizes: true
-        }, take: 5
-    });
-}
+import _ from 'lodash';
+import ProductItemLoading from "@/components/ui/loading/ProductItemLoading";
+import {Suspense} from "react";
 
 export default async function Page() {
-    const products = await getBestSellingProducts();
-    const user = await getCurrentUser();
-
     return (
         <div className='flex flex-col gap-y-6 px-[3vw] py-10 bg-[#F5E6E0]'>
             <h3 className='text-4xl font-bold text-center'>HOT DEALS</h3>
@@ -35,7 +14,13 @@ export default async function Page() {
                     <h5 className='text-2xl font-bold'>Up to 60% Off</h5>
                     <Countdown endDate={new Date(2023, 8, 23)}/>
                 </div>
-                <Products products={products} isAuthenticated={user !== null}/>
+                <Suspense fallback={(
+                    <div className='grid grid-cols-3 md:grid-cols-5 gap-4 w-full'>
+                        {_.range(5).map(i => <ProductItemLoading key={i}/>)}
+                    </div>
+                )}>
+                    <Products/>
+                </Suspense>
             </div>
         </div>
     );
