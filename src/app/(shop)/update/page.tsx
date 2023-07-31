8f0e0eb038575f08de2656abd24b10365f5ab093
EiDemir <ahizadiii@gmail.com>
2023-08-01 00:11:09 +0300

    update

--- src/app/(shop)/update/page.tsx
+++ src/app/(shop)/update/page.tsx
@@ -1,6 +1,8 @@
 import {redirect} from "next/navigation";
 import axios from "axios";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
     axios.get('/api/update').then(() => {});
 
