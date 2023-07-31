a002e7144d847125d9c5fd680dae90b6367305a1
EiDemir <ahizadiii@gmail.com>
2023-08-01 00:21:55 +0300

    update

--- src/app/(shop)/update/page.tsx
+++ src/app/(shop)/update/page.tsx
@@ -1,10 +1,7 @@
 import {redirect} from "next/navigation";
-import axios from "axios";
-
-export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-    axios.get('/api/update').then(() => {});
+    const response = await fetch('https://untitled32.vercel.app/api/update');
 
     redirect('/cart');
 }
