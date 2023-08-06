5767e0367dd3a2e32ed3dd58772ebd7bfecb5f24
EiDemir <ahizadiii@gmail.com>
2023-08-07 02:58:03 +0300

    update

--- src/app/(home)/@hotDeals/page.tsx
+++ src/app/(home)/@hotDeals/page.tsx
@@ -7,7 +7,7 @@ import {Suspense} from "react";
 export default async function Page() {
     return (
         <div className='flex flex-col gap-y-6 p-[3vw] mx-[3vw] bg-[#F5E6E0]/70 backdrop-blur-md rounded-xl drop-shadow-sm'>
-            <h3 className='text-4xl font-bold text-center'>HOT DEALS</h3>
+            <h1 className='text-4xl font-bold text-center'>HOT DEALS</h1>
             <div className='flex lg:flex-row flex-col gap-y-10 gap-x-10 items-center'>
                 <div className='transition-transform translate-y-4'>
                     <h3 className='text-2xl'>Summer Sale</h3>
