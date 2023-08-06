fc8339d9575c721c6daee424c4378a874c1e2dd6
EiDemir <ahizadiii@gmail.com>
2023-08-06 22:53:16 +0300

    update

--- src/app/(home)/MainCategories.tsx
+++ src/app/(home)/MainCategories.tsx
@@ -11,7 +11,7 @@ export default function MainCategories() {
 
     return (
         <div
-            className='flex md:flex-row flex-col gap-x-4 gap-y-4 h-[800px] md:h-[500px] py-10 md:mx-[7vw] mx-[3vw]'>
+            className='flex md:flex-row flex-col gap-x-4 gap-y-4 h-[800px] md:h-[500px] py-10 md:mx-[7vw] mx-[3vw] overflow-x-hidden'>
             <motion.div initial={{opacity: 0, x: -20}} whileInView={{
                 opacity: 1, x: 0, transition: {
                     duration: 0.5
